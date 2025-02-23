35a9a51a2e3d29bfb19b2ed8c2ea557ec494beee
Madhumgit <mithamadhu8785@gmail.com>
2025-02-23 13:52:00 +0530

    update

---
+++ assets/js/sect_sol.js
@@ -0,0 +1,31 @@
+
+function toggleMobileMenu() {
+    const menu = document.querySelector('.mobile-menu');
+    menu.classList.toggle('active');
+  }
+
+  //image animation
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const heroTitle = document.querySelector('.hero h1');
+    heroTitle.classList.add('animate');
+  });
+
+  //color changes for card when hover
+
+  const cards = document.querySelectorAll('.custom-card');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.backgroundColor = 'rgba(0, 123, 255, 0.1)';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.backgroundColor = '';
+  });
+});
+    document.getElementById("captchaBox").addEventListener("click", function () {
+      const checkmark = document.getElementById("checkmark");
+      checkmark.classList.toggle("checked");
+    });
+ 
